d4aa4543751e5915fb5a6085d1ccfb9848e4c89d
prLorence <paulorencereyes@gmail.com>
2021-10-02 20:17:34 +0800

    completed activity #4 input range, used for loops for easy testing

---
+++ CS115/Activity_4_range/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Activity_4_range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Enter a number: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                if (userInput >= 1 & userInput <= 4 | userInput >= 21 & userInput <= 24 | userInput >= 41 & userInput <= 44)
+                {
+                    if (userInput % 2 == 0)
+                    {
+                        Console.WriteLine("AAA");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("BBB");
+                    }
+                }
+
+                else if (userInput >= 11 & userInput <= 14 | userInput >= 31 & userInput <= 34 | userInput >= 51 & userInput <= 54)
+                {
+                    if (userInput % 2 == 0)
+                    {
+                        Console.WriteLine("CCC");
+                    }
+                    else
+                    {
+                        Console.WriteLine("DDD");
+                    }
+                }
+
+                else if (userInput % 5 == 0)
+                {
+                    Console.WriteLine("EEE");
+                }
+
+                else
+                {
+                    Console.WriteLine("FFF");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
